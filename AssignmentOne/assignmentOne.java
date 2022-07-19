f524dfc422fe0cb714bc5020a9459d14b667dd2f
malav-prama <101901228+malav-prama@users.noreply.github.com>
2022-07-19 10:17:33 -0700

    Update assignmentOne.java

--- AssignmentOne/assignmentOne.java
+++ AssignmentOne/assignmentOne.java
@@ -1,7 +1,15 @@
+//To run a java program on cmd prompt
+//cd fileDirectory
+//set path=%path%;C:\Program Files\Java\jdk1.5.0_09\bin
+//javac assignmentOne.java
+//java assignmentOne
+
+
 public class assignmentOne
 {
-public static void main(String[] args)
-{
-System.out.print("Hello World");
-}
+  public static void main(String[] args)
+  {
+    System.out.print("Hello World");
+   }
+
 }
